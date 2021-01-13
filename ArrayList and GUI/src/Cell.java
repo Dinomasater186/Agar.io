bc64484c5c169de9eb9ee6fb81669fb3891f697b
Dinomaster186 <52178070+Dinomasater186@users.noreply.github.com>
2021-01-13 10:35:08 -0800

    Add files via upload

--- ArrayList and GUI/src/Cell.java
+++ ArrayList and GUI/src/Cell.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.util.ArrayList;
 
 public class Cell {
@@ -11,10 +12,14 @@ public class Cell {
 	public double maxSpeed;
 	public double vx;
 	public double vy;
+	public int screenX = 1000;
+	public int screenY = 800;
 	int targetX = 0;
 	int targetY = 0;
 	int cellID;
 	long loopyTimey = 0;
+	boolean amPlayer = false;
+	
 	public Cell (int X, int Y, int ID, int extraMass) {
 		int red = (int)(Math.random() * 256);
 		int green = (int)(Math.random() * 256);
@@ -28,8 +33,15 @@ public class Cell {
 		
 		cellID = ID;
 	}
+	
+	public void setAmPlayer (boolean haveAI) {
+		amPlayer = haveAI;
+	}
+	
 	public void update(ArrayList<Cell> list) {
-		getTarget(list);
+		if (! amPlayer) {
+			getTarget(list);
+		}
 		convertMassToRadius();
 		Move();
 	}
@@ -37,7 +49,7 @@ public class Cell {
 		double distX = targetX - x;
 		double distY = targetY - y;
 		getMaxSpeed();
-		maxSpeed *= loopyTimey;
+		maxSpeed *= loopyTimey/4;
 		double preferedSpeed = Math.sqrt(Math.pow(distX, 2) + Math.pow(distY, 2));
 		double ratio = maxSpeed/preferedSpeed;
 		int moveX = (int)(Math.ceil(Math.abs((distX * ratio))) * Math.signum(distX));
@@ -45,17 +57,30 @@ public class Cell {
 		x += moveX;
 		y += moveY;
 	}
+	
+	public void setY(int Y) {
+		y = Y;
+	}
+	
+	public void setX(int X) {
+		x = X;
+	}
+	
 	public void getMaxSpeed() {
 		maxSpeed = 1/Math.sqrt(mass);
 	}
 	public void convertMassToRadius() {
 		radius = Math.sqrt((mass));
 	}
-	public void paint(Graphics g, ArrayList<Cell> list, int loopTime) {
+	public void paint(Graphics g, ArrayList<Cell> list, int loopTime, double Zoom, int extraX, int extraY) {
 		update(list);
 		loopyTimey = loopTime;
-		g.setColor(c);
-		g.fillOval((int)(x-radius),(int)(y-radius),(int)(radius*2),(int)(radius*2));
+		Rectangle screen = new Rectangle(0,0,1000,900);
+		Rectangle blob = new Rectangle((int)(Zoom * (x-radius - extraX ))+ (500),(int)(Zoom * (y-radius -extraY ))+(425),(int)(Zoom * radius*2),(int)(Zoom * radius*2));
+		if (screen.intersects(blob)) {
+			g.setColor(c);
+			g.fillOval((int)(Zoom * (x-radius - extraX ))+ (500),(int)(Zoom * (y-radius -extraY ))+(425),(int)(Zoom * radius*2),(int)(Zoom * radius*2));
+		}
 	}
 	
 	public double getDistance(int TargetX, int TargetY) {
@@ -83,19 +108,30 @@ public class Cell {
 		int initialTargetX = 0;
 		int initialTargetY = 0;
 		int initialTargetMass = 0;
-		for (int i = 0; i < Math.min(2,list.size()); i ++) {
-			if (cellID != i) {
+		for (int i = 0; i < list.size(); i ++) {
+			if (cellID != i && mass < list.get(i).getMass()) {
 				initialTargetX = list.get(i).getX();
 				initialTargetY = list.get(i).getY();
-				initialTargetMass =  list.get(i).getMass();
+				initialTargetMass =  5;
 			}
 		}
-		double targetScore = initialTargetMass / Math.pow(getDistance(initialTargetX,initialTargetY)/radius,2);
+		double targetScore = Math.pow(initialTargetMass,2) / Math.pow(getDistance(initialTargetX,initialTargetY),3);
 		for (int i = 0 ; i < list.size(); i ++) {
-			if (cellID != list.get(i).getCellID() && list.get(i).getMass()<mass && targetScore <= list.get(i).getMass()/Math.pow(getDistance(list.get(i).getX(),list.get(i).getY())/radius,2)) {
+			if (cellID != list.get(i).getCellID() && list.get(i).getMass()<mass && targetScore <= Math.pow(list.get(i).getMass(),2)/Math.pow(getDistance(list.get(i).getX(),list.get(i).getY()),3)) {
 				targetX = list.get(i).getX(); initialTargetX = list.get(i).getX();
 				targetY = list.get(i).getY(); initialTargetY = list.get(i).getY();
-				targetScore = initialTargetMass / Math.pow(getDistance(initialTargetX,initialTargetY)/radius,2);
+				initialTargetMass = list.get(i).getMass();
+				targetScore = Math.pow(initialTargetMass,2) / Math.pow(getDistance(initialTargetX,initialTargetY),3);
+			}
+		}
+		if (targetScore*10000 <= 1) {
+			double dist = getDistance(initialTargetX,initialTargetY);
+			for (int i = 0 ; i < list.size(); i ++) {
+				if (cellID != list.get(i).getCellID() && list.get(i).getMass()<mass && dist >= getDistance(list.get(i).getX(),list.get(i).getY())) {
+					targetX = list.get(i).getX(); initialTargetX = list.get(i).getX();
+					targetY = list.get(i).getY(); initialTargetY = list.get(i).getY();
+					dist = getDistance(initialTargetX,initialTargetY);
+				}
 			}
 		}
 	}
--- ArrayList and GUI/src/Driver.java
+++ ArrayList and GUI/src/Driver.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,34 +16,54 @@ public class Driver extends JPanel implements MouseListener, ActionListener {
 	Timer t;
 	Long lastLoopTime = System.currentTimeMillis();
 	int extraCellID = 100;
+	int numPlayers = 0;
+	Font big = new Font("Courier New", 1, 50);
+	Font small = new Font("Courier New", 1, 30);
+	Font biggest = new Font("Courier New", 1, 90);
+	int z = 0;
+	
+	public void reset() {
+		z = 0;
+		Enimy.clear();
+		for (int i = 0; i < 100; i ++) {
+			Cell c = new Cell((int)(Math.random()*1000),(int)(Math.random()*1000),i, 50);
+			Enimy.add(c);
+		}
+		
+		for (int i = 0; i < 200; i ++)  {
+			Cell c = new Cell((int)(Math.random()*1000),(int)(Math.random()*1000),extraCellID, 5);
+			Enimy.add(c);
+		}
+	}
 	
 	public Driver() {
 		JFrame frame = new JFrame("Agar.io");
 		frame.setSize(1000, 1000);
 		frame.add(this);
 		
-		for (int i = 0; i < 40; i ++) {
-			Cell c = new Cell((int)(Math.random()*1000),(int)(Math.random()*1000),i, 200);
-			Enimy.add(c);
-		}
+		reset();
 		
-		for (int i = 0; i < 500; i ++)  {
-			Cell c = new Cell((int)(Math.random()*1000),(int)(Math.random()*1000),extraCellID, 50);
-			Enimy.add(c);
-		}
 		t = new Timer(10,this);
 		t.start();
+		
+		
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}
 	public void paint(Graphics g) {
+		numPlayers = 0;
 		super.paintComponent(g);
 		int loopTime = (int)(System.currentTimeMillis() - lastLoopTime);
-		System.out.println(loopTime);
 		lastLoopTime = System.currentTimeMillis();
+		
+		double zoom = Math.max(600/(Math.pow((double)Enimy.get(0).getMass(),0.9)),1);
+		
+		g.drawRect((int)( zoom * -1 * Enimy.get(0).getX()) + 500, (int)(zoom * -1 * Enimy.get(0).getY()) + 425, (int)(1000*zoom), (int)(1000*zoom));
+		
 		for (Cell e : Enimy) {
-			e.paint(g,Enimy,loopTime);
+			
+			e.paint(g,Enimy,loopTime,zoom,Enimy.get(0).getX(),Enimy.get(0).getY());
 			for (int i = 0; i < Enimy.size(); i ++) {
 				Cell j = Enimy.get(i);
 				if (e.getMass() > j.getMass() && e.getDistance(j.getX(), j.getY()) <= e.getRadius()) {
@@ -50,11 +72,45 @@ public class Driver extends JPanel implements MouseListener, ActionListener {
 					i --;
 				}
 			}
+			
+			if (e.getCellID() != extraCellID) {
+				numPlayers ++;
+			}
+			
+			if (e.getX() + e.radius > 1000) {e.setX((int)(1000-e.radius -1));}
+			if (e.getY() + e.radius > 1000) {e.setY((int)(1000-e.radius -1));}
+			if (e.getX() - e.radius < 0) {e.setX((int)(e.radius + 1));}
+			if (e.getY() - e.radius < 0) {e.setY((int)(e.radius + 1));}
+			
+		}
+		if (Enimy.size() <= 10000) {
+			for (int i = 0; i < 1; i ++)  {
+				Cell c = new Cell((int)(Math.random()*1000),(int)(Math.random()*1000),extraCellID, 5);
+				Enimy.add(c);
+			}
 		}
 		
-		for (int i = 0; i < 1; i ++)  {
-			Cell c = new Cell((int)(Math.random()*1000),(int)(Math.random()*1000),extraCellID, 50);
-			Enimy.add(c);
+		if (numPlayers == 1) {
+			z ++;
+			g.setColor(Color.black);
+			g.setFont(biggest);
+			if (Enimy.get(0).getCellID() != 0) {
+				g.drawString("Cell " + (Enimy.get(0).getCellID() + 1) + " Won", 220, 300);
+			}
+			else {
+				g.drawString("You Won", 300, 300);
+			}
+		}
+		if (z == 100) {
+			reset();
+		}
+
+		g.setColor(Color.black);
+		g.setFont(small);
+		g.drawString("Cells Remaining " + numPlayers, 660, 50);
+		g.setFont(small);
+		if (Enimy.get(0).getCellID() != 0) {
+			g.drawString("Spectating " + (Enimy.get(0).getCellID() + 1), 0, 900);
 		}
 		
 	}
